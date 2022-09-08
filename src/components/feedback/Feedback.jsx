2c81e2300c073f0c7347cb83035a665cc6345e05
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2022-09-08 17:00:30 +0300

    add statistic displaying

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -2,30 +2,45 @@ import React, { Component } from 'react';
 
 class Feedback extends Component {
   state = this.props.initialState;
-  buttonClickHandler = e => {
-    console.log('click on Good button', e.currentTarget.children.value);
+  buttonClickHandler = event => {
+    console.log('click on any button', event.target.name);
+    this.setState(prevState => {
+      return {
+        [event.target.name]: prevState[event.target.name] + 1,
+      };
+    });
   };
   render() {
     return (
       <div className="feedback">
         <div className="feedbackControl">
           <span>Please leave your feedback</span>
-          <button type="button" onClick={this.buttonClickHandler}>
+          <button type="button" name="good" onClick={this.buttonClickHandler}>
             Good
           </button>
-          <button type="button" onClick={this.buttonClickHandler}>
+          <button
+            type="button"
+            name="neutral"
+            onClick={this.buttonClickHandler}
+          >
             Neutral
           </button>
-          <button type="button" onClick={this.buttonClickHandler}>
+          <button type="button" name="bad" onClick={this.buttonClickHandler}>
             Bad
           </button>
         </div>
         <div>
           <span>Statistics</span>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+              <span>{this.state.good}</span>
+            </li>
+            <li>
+              <span>{this.state.neutral}</span>
+            </li>
+            <li>
+              <span>{this.state.bad}</span>
+            </li>
           </ul>
         </div>
       </div>
