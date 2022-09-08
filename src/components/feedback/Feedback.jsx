4bc53180f4d7375410ea9e89811cdef89ec5ddcc
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2022-09-08 14:09:50 +0300

    basic on click handlers

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 
 class Feedback extends Component {
   state = this.props.initialState;
+  buttonClickHandler = e => {
+    console.log('click on Good button', e.currentTarget.children.value);
+  };
   render() {
     return (
       <div className="feedback">
         <div className="feedbackControl">
           <span>Please leave your feedback</span>
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          <button type="button" onClick={this.buttonClickHandler}>
+            Good
+          </button>
+          <button type="button" onClick={this.buttonClickHandler}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.buttonClickHandler}>
+            Bad
+          </button>
         </div>
         <div>
           <span>Statistics</span>
